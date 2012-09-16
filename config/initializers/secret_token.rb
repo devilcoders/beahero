ca74b45d65353b4ebb2a0cfffa62f863592ab357
Alexey Topolyanskiy <devilcoders@gmail.com>
2012-09-16 14:09:59 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beahero::Application.config.secret_token = 'bcde1cf917c4c1bf3bd0dba89da3fdd400af73f2e8a05632ef81fe4435b60445e6d15a4f69a6834ca195558c779dde913e4a708d9e0fa608b2e3d3394f5b11c8'
